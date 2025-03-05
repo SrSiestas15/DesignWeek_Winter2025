31e3e18739b753d8b3d0804927b80a4c71c0a56a
SrSiestas15 <148352083+SrSiestas15@users.noreply.github.com>
2025-03-05 17:39:05 -0500

    worked on sprites

--- DesignWeek_Winter2025/Assets/CombinationReader.cs
+++ DesignWeek_Winter2025/Assets/CombinationReader.cs
@@ -11,6 +11,8 @@ public class CombinationReader : MonoBehaviour
     static List<Vector2> slotValues;
     public static int roomChosen;
     public List<string> possibleCombinations;
+    public List<GameObject> possibleRobots;
+    public static string currentCode;
 
     // Start is called before the first frame update
     void Start()
@@ -21,35 +23,56 @@ public class CombinationReader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public static void CheckChildren()
     {
         slotValues = new List<Vector2>();
-        foreach(TokenSlot slot in slots)
+        foreach (TokenSlot slot in slots)
         {
-            if(slot.GetComponentInChildren<DraggableItem>() != null)
+            if (slot.GetComponentInChildren<DraggableItem>() != null)
             {
                 DraggableItem draggableItem = slot.gameObject.GetComponentInChildren<DraggableItem>();
                 slotValues.Add(new Vector2(slot.slotID, draggableItem.draggableID));
             }
         }
 
-        checkRoom();
+        CheckRoom();
+        GetCode();
     }
 
-    public static void checkRoom()
+    public static void CheckRoom()
     {
         for (int i = 0; i < slotValues.Count; i++)
         {
             if (slotValues[i].y == 1)
             {
                 roomChosen = Mathf.RoundToInt(slotValues[i].x);
+                slotValues.Remove(slotValues[i]);
             }
         }
-
-        Debug.Log("Chosen Room: "+roomChosen);
     }
 
+    public static void GetCode()
+    {
+        currentCode = string.Empty;
+
+        for (int i = 0; i < slotValues.Count; i++)
+        {
+            currentCode += slotValues[i].x;
+            currentCode += slotValues[i].y;
+        }
+    }
+
+    public void RoomAndRobot()
+    {
+        for (int i = 0; i < possibleCombinations.Count; i++)
+        {
+            if (possibleCombinations[i] == currentCode)
+            {
+                Debug.Log($"move to room {roomChosen} and spawn robot {i}");
+            }
+        }
+    }
 }
